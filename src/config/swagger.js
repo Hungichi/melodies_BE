c1043596f593c582b8ef05c32b341d8d533e416b
Nguyen-thuc526 <nguyennt.itse@gmail.com>
2025-04-03 10:13:43 +0700

    #3: Add link production

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -3,44 +3,48 @@ const path = require('path');
 
 const options = {
 
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Melodies API Documentation',
-      version: '1.0.0',
-      description: 'API documentation for Melodies music application',
-    },
-    servers: [
-      {
-        url: 'https://be-melodies-dch6fbbo0-nguyens-projects-9f74018a.vercel.app',
-        description: 'Development server',
-      },
-      {
-        url: 'http://localhost:8000',
-        description: 'Alternative development server',
-      }
-    ],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Melodies API Documentation',
+            version: '1.0.0',
+            description: 'API documentation for Melodies music application',
         },
-        security: [
+        servers: [
             {
-                bearerAuth: [],
+                url: 'https://be-melodies-ggf27rs8a-nguyens-projects-9f74018a.vercel.app',
+                description: 'Production Server',
             },
+            {
+                url: 'http://localhost:5000',
+                description: 'Development server',
+            },
+            {
+                url: 'http://localhost:8000',
+                description: 'Alternative development server',
+            }
         ],
-    },
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
 
-    security: [{
-      bearerAuth: [],
-    }],
-  },
-  apis: [path.resolve(__dirname, '../routes/*.js')], // Absolute path to the API routes
+                },
+                security: [
+                    {
+                        bearerAuth: [],
+                    },
+                ],
+            },
+
+            security: [{
+                bearerAuth: [],
+            }],
+        },
+        apis: [path.resolve(__dirname, '../routes/*.js')], // Absolute path to the API routes
 
 
-const specs = swaggerJsdoc(options);
-module.exports = specs;
+        const specs = swaggerJsdoc(options);
+        module.exports = specs;
